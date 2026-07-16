7753c237a45fef7b76abffe2141343c70aa9e556
ElmazMed <elmazgour.mohamed@gmail.com>
2026-07-17 02:54:20 +0300

    Installed all the needed packages for the backend and created the server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.listen(PORT, ()=>{console.log(PORT);
+})
