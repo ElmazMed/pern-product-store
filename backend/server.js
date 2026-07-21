ee5afc464578d7510694c51f9c13e5f2629ebb42
ElmazMed <elmazgour.mohamed@gmail.com>
2026-07-22 00:50:17 +0300

    Sat up controllers and DB connection

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors"
 import dotenv from "dotenv"
+import productsRoute from "./routes/productsRoute.js"
 
 dotenv.config()
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use("/api/products", productsRoute)
 
 app.listen(PORT, ()=>{console.log(PORT);
 })
