c2744779cb7aeac8053a35f6c2e071f6617a5117
ElmazMed <elmazgour.mohamed@gmail.com>
2026-07-27 23:18:38 +0300

    created post and get products api's

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,3 +1,50 @@
-export const getAllProducts = async (req, res)=>{}
+import { sql } from "../config/db.js";
 
-export const createProduct = async (req, res)=>{}
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await sql`
+        SELECT * FROM products
+        ORDER BY created_at DESC
+        `;
+
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: "An error in getAllProducts controller",
+    });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const { name, image, price } = req.body;
+
+  if (!name || !image || !price) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All the fields are required" });
+  }
+
+  try {
+    const newProduct = await sql`
+        INSERT INTO products (name, image, price)
+        VALUES (${name}, ${image}, ${price})
+        RETURNING *
+        `;
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log(error);
+
+    res.status(400).json({
+      success: false,
+      message: "An error in createProduct controller",
+    });
+  }
+};
+
+export const getProduct = async (req, res) => {};
+
+export const updateProduct = async (req, res) => {};
+
+export const deleteProduct = async (req, res) => {};
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { createProduct, getAllProducts } from "../controllers/productControllers.js";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProduct,
+  updateProduct,
+} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
+router.get("/:id", getProduct);
 router.post("/", createProduct);
-
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
