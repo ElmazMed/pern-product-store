c5cba4962d27423f385e86699c675434d5faa112
ElmazMed <elmazgour.mohamed@gmail.com>
2026-07-28 00:40:36 +0300

    completed the creation of  all api's

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -32,7 +32,7 @@ export const createProduct = async (req, res) => {
         VALUES (${name}, ${image}, ${price})
         RETURNING *
         `;
-    res.status(201).json({ success: true, data: newProduct });
+    res.status(201).json({ success: true, data: newProduct[0] });
   } catch (error) {
     console.log(error);
 
@@ -43,8 +43,71 @@ export const createProduct = async (req, res) => {
   }
 };
 
-export const getProduct = async (req, res) => {};
+export const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-export const updateProduct = async (req, res) => {};
+    const product = await sql`
+        SELECT * FROM products WHERE id = ${id}
+        `;
 
-export const deleteProduct = async (req, res) => {};
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (error) {
+    console.log(error);
+
+    res.status(400).json({
+      success: false,
+      message: "An error in getProduct controller",
+    });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, image, price } = req.body;
+
+  try {
+    const updatedProduct = await sql`
+        UPDATE products SET name=${name}, image=${image}, price=${price} WHERE id=${id} RETURNING *
+        `;
+    if (updateProduct.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Product cannot be found",
+      });
+    }
+
+    res.status(200).json({ success: true, data: updatedProduct[0] });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "An error in updateProduct controller",
+    });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const removeProduct = await sql`
+    DELETE FROM products WHERE id = ${id}
+    `;
+    if (deleteProduct.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Product cannot be found",
+      });
+    }
+    res
+      .status(200)
+      .json({ success: true, message: "Product removed succefully!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error in deleteProcut controller",
+    });
+  }
+};
