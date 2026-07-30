9f25eed0962443fbf43a679b43dfcb8503523a81
ElmazMed <elmazgour.mohamed@gmail.com>
2026-07-30 23:09:17 +0300

    Added rate limiting securtiy using Arcjet

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,17 @@
+import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
+import "dotenv/config";
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({ mode: "LIVE", allow: ["CATEGORY:SEARCH_ENGINE"] }),
+    tokenBucket({
+      mode: "LIVE",
+      refilRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import productsRoute from "./routes/productsRoute.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -15,6 +16,38 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, { requested: 1 });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res
+          .status(429)
+          .json({ error: "Too many requests, please try again later" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed(),
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
+
 app.use("/api/products", productsRoute);
 
 async function initDB() {
